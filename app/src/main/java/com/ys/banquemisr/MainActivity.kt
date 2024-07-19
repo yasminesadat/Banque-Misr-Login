cd2103c43d638ec989626d93947f41c3792ae964
Yasmine Elsadat <yasmine.atef2004@gmail.com>
2024-07-19 19:43:32 +0300

    localeBugFix

--- app/src/main/java/com/ys/banquemisr/MainActivity.kt
+++ app/src/main/java/com/ys/banquemisr/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.ys.banquemisr
 
+import android.app.LocaleManager
+import android.content.Context
+import android.graphics.drawable.Icon
+import android.os.Build
 import android.os.Bundle
+import android.os.LocaleList
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -29,23 +37,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.os.LocaleListCompat
 import com.ys.banquemisr.ui.theme.Cherry
 import com.ys.banquemisr.ui.theme.Gray
 import com.ys.banquemisr.ui.theme.LightCherry
@@ -55,13 +62,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-                LogIn()
+            LogIn()
         }
     }
 }
 
 @Composable
 fun LogIn(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    var usernameField by remember { mutableStateOf("") }
+    var passwordField by remember { mutableStateOf("") }
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -76,51 +87,36 @@ fun LogIn(modifier: Modifier = Modifier) {
                 painter = painterResource(id = R.drawable.bm_icon),
                 contentDescription = stringResource(R.string.banque_misr_icon)
             )
-            TextButton(onClick = { /* Your button click logic here */ }) {
+            TextButton(onClick = {
+                changeLocales(context)
+            }) {
                 Text(
-                    text = stringResource(R.string.ar_translation),
+                    text = stringResource(R.string.language),
                     fontWeight = FontWeight.Bold,
                     color = Cherry,
-                    fontSize =16.sp,
+                    fontSize = 16.sp,
                     fontFamily = FontFamily(Font(R.font.change, FontWeight.Normal))
                 )
             }
         }
-        var usernameField by remember { mutableStateOf("Username") }
-        var passwordField by remember { mutableStateOf("Password") }
-        var showUsernameLabel by remember { mutableStateOf(false) }
-        var showPasswordLabel by remember { mutableStateOf(false) }
-        var isFocusedUser by remember { mutableStateOf(false) }
-        var isFocusedPass by remember { mutableStateOf(false) }
+
         OutlinedTextField(
             value = usernameField,
             onValueChange = { usernameField = it },
             label = {
-                if (showUsernameLabel)
-                    Text(
-                        text = "Username",
-                        color = Color.DarkGray
-                    )
+                Text(
+                    text = stringResource(id = R.string.username),
+                    color = Color.Gray
+                )
             },
             modifier = modifier
                 .align(Alignment.CenterHorizontally)
                 .padding(top = 36.dp, bottom = 16.dp)
                 .fillMaxWidth()
-                .height(64.dp)
-                .onFocusChanged {
-                    isFocusedUser = it.isFocused && !isFocusedUser
-                    if (isFocusedUser) {
-                        showUsernameLabel = true
-                        if (usernameField == "Username")
-                            usernameField = ""
-                    }
-                },
-            textStyle =
-            if (usernameField == "Username") TextStyle(Color.LightGray, fontSize = 16.sp)
-            else TextStyle(Color.Black, fontSize = 16.sp, fontWeight = FontWeight.Bold),
+                .height(64.dp),
+            textStyle = TextStyle(Color.Black, fontSize = 16.sp, fontWeight = FontWeight.Bold),
             colors = androidx.compose.material3.OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.Gray,
-                unfocusedBorderColor = Color.LightGray
+                focusedBorderColor = Color.Gray, unfocusedBorderColor = Color.LightGray
             )
         )
         var passwordVisible by remember { mutableStateOf(false) }
@@ -128,36 +124,23 @@ fun LogIn(modifier: Modifier = Modifier) {
             value = passwordField,
             onValueChange = { passwordField = it },
             label = {
-                if (showPasswordLabel)
-                    Text(
-                        text = "Password",
-                        color = Color.DarkGray
-                    )
+                Text(
+                    text = stringResource(R.string.password),
+                    color = Color.Gray
+                )
             },
             modifier = modifier
                 .align(Alignment.CenterHorizontally)
                 .fillMaxWidth()
-                .height(64.dp)
-                .onFocusChanged {
-                    isFocusedPass = it.isFocused && !isFocusedPass
-                    if (isFocusedPass) {
-                        showPasswordLabel = true
-                        if (passwordField == "Password")
-                            passwordField = ""
-                    }
-                },
-            textStyle =
-            if (passwordField == "Password") TextStyle(Color.LightGray, fontSize = 16.sp)
-            else TextStyle(Color.Black, fontSize = 16.sp, fontWeight = FontWeight.Bold),
+                .height(64.dp),
+            textStyle = TextStyle(Color.Black, fontSize = 16.sp, fontWeight = FontWeight.Bold),
             colors = androidx.compose.material3.OutlinedTextFieldDefaults.colors(
-                focusedBorderColor = Color.Gray,
-                unfocusedBorderColor = Color.LightGray
+                focusedBorderColor = Color.Gray, unfocusedBorderColor = Color.LightGray
             ),
             trailingIcon = {
-                IconButton(
-                    onClick = {
-                        passwordVisible = !passwordVisible
-                    }) {
+                IconButton(onClick = {
+                    passwordVisible = !passwordVisible
+                }) {
                     Image(
                         painter = painterResource(id = if (passwordVisible) R.drawable.visible else R.drawable.not_visible),
                         contentDescription = "password_visibility",
@@ -166,7 +149,7 @@ fun LogIn(modifier: Modifier = Modifier) {
                 }
             },
             visualTransformation =
-            if (passwordVisible || passwordField == "Password") VisualTransformation.None else PasswordVisualTransformation(),
+            if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
         )
         Row(
             modifier = Modifier
@@ -175,7 +158,7 @@ fun LogIn(modifier: Modifier = Modifier) {
             horizontalArrangement = Arrangement.End,
         ) {
             Text(
-                text = "Forgot password/username?",
+                text = stringResource(R.string.forgot_password_username),
                 textDecoration = TextDecoration.Underline,
                 fontSize = 12.sp,
                 color = Gray
@@ -191,18 +174,15 @@ fun LogIn(modifier: Modifier = Modifier) {
             shape = RoundedCornerShape(8.dp),
 
             colors = ButtonDefaults.buttonColors(
-                containerColor =
-                if (usernameField != "Username" && usernameField.isNotBlank()
-                    && passwordField != "Password" && passwordField.isNotBlank()
-                )
-                    Cherry else LightCherry,
+                containerColor = if (usernameField.isNotBlank() && passwordField.isNotBlank()) Cherry else LightCherry,
             )
 
         ) {
             Text(
-                text = "LOG IN",
+                text = stringResource(R.string.log_in),
                 fontWeight = FontWeight.Bold,
-                fontSize = 16.sp
+                fontSize = 16.sp,
+                fontFamily = FontFamily(Font(R.font.change, FontWeight.Normal))
             )
         }
         SuggestionHyperlinks()
@@ -225,12 +205,10 @@ fun SuggestionHyperlinks() {
         horizontalArrangement = Arrangement.End
     ) {
         Text(
-            text = "Schedule/Reschedule ",
-            fontSize = 12.sp,
-            color = Gray
+            text = stringResource(R.string.schedule_reschedule), fontSize = 12.sp, color = Gray
         )
         Text(
-            text = "Branch Visit",
+            text = stringResource(R.string.branch_visit),
             textDecoration = TextDecoration.Underline,
             fontSize = 12.sp,
             color = Cherry,
@@ -245,12 +223,10 @@ fun SuggestionHyperlinks() {
         horizontalArrangement = Arrangement.End,
     ) {
         Text(
-            text = "New to the app? ",
-            fontSize = 12.sp,
-            color = Gray
+            text = stringResource(R.string.new_to_the_app), fontSize = 12.sp, color = Gray
         )
         Text(
-            text = "Register",
+            text = stringResource(R.string.register),
             textDecoration = TextDecoration.Underline,
             fontSize = 12.sp,
             color = Cherry,
@@ -260,67 +236,32 @@ fun SuggestionHyperlinks() {
 }
 
 @Composable
-fun FeaturesAndIcons(){
+fun FeaturesAndIcons() {
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 14.dp),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.loan_calc),
-            contentDescription = null,
-            modifier = Modifier.size(56.dp)
-        )
-
-        Image(
-            painter = painterResource(id = R.drawable.our_products),
-            contentDescription = null,
-            modifier = Modifier.size(56.dp)
-        )
-
-
-        Image(
-            painter = painterResource(id = R.drawable.nearest_branch_or_atm),
-            contentDescription = null,
-            modifier = Modifier.size(56.dp)
-        )
-
-        Image(
-            painter = painterResource(id = R.drawable.exchange_rate),
-            contentDescription = null,
-            modifier= Modifier.size(56.dp)
-        )
-
-
+        AppFeature(feature = R.string.loan_calculator, icon = R.drawable.loan_calc)
+        AppFeature(feature = R.string.our_products, R.drawable.our_products)
+        AppFeature(feature = R.string.branch_atm, icon = R.drawable.nearest_branch_or_atm)
+        AppFeature(feature = R.string.exchange_rates, icon = R.drawable.exchange_rate)
     }
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 1.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ){
-        Text(
-            text = "Loan\ncalculator",
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-            fontSize = 12.sp,
-            lineHeight = 14.sp
+}
+
+@Composable
+fun AppFeature(@StringRes feature: Int, @DrawableRes icon: Int, modifier: Modifier = Modifier) {
+    Column(
+
+    ) {
+        Image(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            modifier = Modifier.size(56.dp)
         )
         Text(
-            text = "Our products",
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-            fontSize = 12.sp
-        )
-        Text(
-            text = "Branch/ATM",
-            textAlign = TextAlign.Center,
-            color = Color.Black,
-            fontSize = 12.sp
-        )
-        Text(
-            text = "Exchange\nrates",
+            text = stringResource(feature),
             textAlign = TextAlign.Center,
             color = Color.Black,
             fontSize = 12.sp,
@@ -328,9 +269,26 @@ fun FeaturesAndIcons(){
         )
     }
 }
+
+fun changeLocales(context: Context) {
+    val locale = context.resources.configuration.locales[0]
+    val localeString = if (locale.language == "ar") "en" else "ar"
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        val localeManager = context.getSystemService(LocaleManager::class.java)
+        localeManager.applicationLocales = LocaleList.forLanguageTags(localeString)
+    } else {
+        AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(localeString))
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun LogInPreview() {
     LogIn()
 }
+
+
+
+
